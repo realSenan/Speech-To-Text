2058a09b1640e1b1149c930441bff392080c2958
Senan <senanm260@gmail.com>
2024-04-04 01:50:14 +0400

    added new dream

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,45 +1,64 @@
-import React, { useContext } from 'react'
-import { LangContext, TranscriptContext } from '../App'
-import { HiMiniClipboardDocumentList } from 'react-icons/hi2'
-import { GrPowerReset } from 'react-icons/gr'
-import toast, { Toaster } from 'react-hot-toast'
+import React, { useContext } from 'react';
+import { LangContext, TranscriptContext } from '../App';
+import { HiMiniClipboardDocumentList } from 'react-icons/hi2';
+import { GrPowerReset } from 'react-icons/gr';
+import toast, { Toaster } from 'react-hot-toast';
 
 const TextArea: React.FC = () => {
-    const { transcript, setTranscript } = useContext(TranscriptContext)
-    const { setLang } = useContext(LangContext)
+    const { transcript, setTranscript } = useContext(TranscriptContext);
+    const { setLang, lang } = useContext(LangContext);
 
     const languageHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const selectedOption = e.target.selectedOptions[0]
-        setLang(e.target.value)
-        toast.success(`Changed successfully: ${selectedOption.innerText}`)
-    }
+        const selectedOption = e.target.selectedOptions[0];
+        setLang(e.target.value);
+
+        const successMessage =
+            e.target.value === 'en'
+                ? `Changed successfully: ${selectedOption.innerText}`
+                : e.target.value === 'tr'
+                  ? `Başarıyla değiştirildi: ${selectedOption.innerText}`
+                  : `Uğurla dəyişdirildi: ${selectedOption.innerText}`;
+        toast.success(successMessage);
+    };
 
     const resetValue = () => {
-        setTranscript('')
-        toast.success('Text reset!')
-    }
+        setTranscript('');
+        const resetMessage =
+            lang === 'en'
+                ? 'Text reset!'
+                : lang === 'tr'
+                  ? 'Metin sıfırlandı!'
+                  : 'Mətn sıfırlandı!';
+        toast.success(resetMessage);
+    };
 
     const copyClipBoard = async () => {
         try {
-            await navigator.clipboard.writeText(transcript)
-            toast.success('Text copied to clipboard!')
+            await navigator.clipboard.writeText(transcript);
+            const copyMessage =
+                lang === 'en'
+                    ? 'Text copied to clipboard!'
+                    : lang === 'tr'
+                      ? 'Metin panoya kopyalandı!'
+                      : 'Mətn panoya kopyalandı!';
+            toast.success(copyMessage);
         } catch (error) {
-            console.error(error)
+            console.error(error);
         }
-    }
+    };
 
     return (
-        <div className="bg-neutral-800 p-10 h-1/2">
-            <div className="container mx-auto duration-300 h-full">
+        <div className="h-1/2 bg-neutral-800 p-10">
+            <div className="container mx-auto h-full duration-300">
                 <Toaster position="top-center" reverseOrder={false} />
                 <div className="right-10 -mt-6 mb-2 ms-auto flex h-10 max-w-fit items-center gap-3 rounded-lg ">
                     <select
                         className="h-10 cursor-pointer  rounded-lg px-4 outline-none"
                         onChange={languageHandler}
                     >
-                        <option value="az-AZ">Azerbaijan</option>
-                        <option value="tr-TR">Turkish</option>
-                        <option value="en-EN">English</option>
+                        <option value="az">Azerbaijan</option>
+                        <option value="tr">Turkish</option>
+                        <option value="en">English</option>
                     </select>
 
                     <button
@@ -57,14 +76,14 @@ const TextArea: React.FC = () => {
                     </button>
                 </div>
                 <textarea
-                placeholder='Just talk about something...'
+                    placeholder="Just talk about something..."
                     onChange={(e) => setTranscript(e.target.value)}
                     className=" h-full w-full overscroll-y-auto rounded-lg bg-neutral-700/20 p-3 text-xl outline-none"
                     value={transcript.trim()}
                 />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TextArea
+export default TextArea;
